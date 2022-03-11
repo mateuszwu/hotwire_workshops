eed5d3714e0ea386a35026cf44aab5ea8f7a0d48
Mateusz Wieczorek <mateusz.wu@gmail.com>
2022-03-11 13:41:03 +0100

    turbo stream for game edit

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -25,8 +25,19 @@ class GamesController < ApplicationController
   def update
     game = Game.find(params[:id])
     game.update(game_params)
+    # player = game.players.find_by(user_id: current_user.id)
 
-    redirect_to games_path
+    game.players.each do |player|
+      Turbo::StreamsChannel.broadcast_action_to(
+        "game_id_#{game.id}-player_id#{player.id}", # na jaki kanal ma isc odpowiedz app/views/games/show.html.erb:1
+        action: :update, # co sie ma stac, czy frame ma byc zupdatdowany, czy ma byc usuniety... https://github.com/hotwired/turbo-rails/blob/main/app/models/concerns/turbo/broadcastable.rb
+        target: game, # tutaj okreslamy jaki frame ma zostac zupdatowany app/views/games/_game.erb:1
+        partial: 'games/game', # jaki partial ma byc wyrederowany
+        locals: { game: game, user: player.user, player: player } # jakie paramsy maja trafic do partiala
+      )
+    end
+
+    redirect_to game
   end
 
   def edit
--- app/models/game.rb
+++ app/models/game.rb
@@ -7,4 +7,11 @@ class Game < ApplicationRecord
   belongs_to :current_place, class_name: 'Place', foreign_key: :current_place_id, optional: true
 
   validates :title, :password, :create_user_id, presence: true
+  after_update_commit do |game|
+    # broadcast_update_to :game_show, target: game, locals: { game: self, user: User.current, player: User.current.players.find_by(game_id: self.id) }
+    # game.players.each do |player|
+    #   broadcast_update_to game, target: "game_id_#{game.id}-player_id#{player.id}", locals: { game: self, user: User.current, player: User.current.players.find_by(game_id: self.id) }
+    #   # game.broadcast_action(:update, locals: { user: player.user, player: player })
+    # end
+  end
 end
